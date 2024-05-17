a93eae446a30a62e2fa8dd07cfdc78ce231c2ca0
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-05-17 20:36:41 +0300

    login funcction

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,4 +1,18 @@
 const Login = () => {
+  function loginUser(email, password) {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Successfully logged in
+        const user = userCredential.user;
+        console.log("User logged in:", user);
+      })
+      .catch((error) => {
+        console.error("Error during login:", error.message);
+      });
+  }
+
   return (
     <div>
       <input type="text" placeholder="Username" />
